cb1107685273e2fd9333aede51ecb7358963638e
Ibimina hart <ibimina.c.hart@gmail.com>
2023-08-06 14:19:19 +0100

    docs: export image files

---
+++ assets/index.ts
@@ -0,0 +1,6 @@
+export const ImagesAssets = {
+    google: require('./images/icons8-google.svg'),
+    github: require('./images/icons8-github.svg'),
+    fav: require('./images/favicon.png'),
+    logo: require('./images/logo.png'),
+}
